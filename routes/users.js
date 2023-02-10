bfd8f3eaa9793b37a10804712adaf06c4a552a70
pujaKundu <iampujakundu@gmail.com>
2023-02-10 21:12:24 +0600

    update user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt")
 
-router.get("/", (req, res) => {
-    res.send("la la la")
-})
+//update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.getSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password,salt)
+            }
+            catch (error) {
+                return res.status(500).json(error)
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            })
+            res.status(200).json("Account has been updated successfully")
+        }
+        catch (error) {
+            return res.status(500).json(error);
+        }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
+});
+//delete user
+//get user
+//follow user
+//unfollow user
 
 module.exports = router;
