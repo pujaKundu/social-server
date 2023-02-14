a523cfb31d91ce54c4dee5b4d0345a953238f665
pujaKundu <iampujakundu@gmail.com>
2023-02-14 23:58:56 +0600

    edit

--- routes/posts.js
+++ routes/posts.js
@@ -75,16 +75,16 @@ router.get("/:id", async (req, res) => {
 });
 //get newsfeed posts
 
-router.get("/newsfeed/all", async (req, res) => {
+router.get("/newsfeed/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
         return Post.find({ userId: friendId });
       })
     );
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
