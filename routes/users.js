7c92782b5b4d79d77d3780909bccbce0066b0150
pujaKundu <iampujakundu@gmail.com>
2023-02-17 23:18:44 +0600

    id

--- routes/users.js
+++ routes/users.js
@@ -40,14 +40,21 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-//get a user
+//get a user by id
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+//get a user by username
 router.get("/", async (req, res) => {
-  const userId = req.query.userId;
   const username = req.query.username;
   try {
-    const user = userId
-      ? await User.findById(userId)
-      : await User.findOne({ username: username });
+    const user = await User.findOne({ username: username });
     const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
   } catch (err) {
