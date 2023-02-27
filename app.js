6fec09f8c06af03e857f1d264aee788b6085b455
pujaKundu <iampujakundu@gmail.com>
2023-02-28 00:26:44 +0600

    file upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer=require('multer')
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -28,6 +29,24 @@ mongoose
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null,'public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null,file.originalname)
+  }
+})
+
+const upload = multer({storage})
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('File uploaded successfully')
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
