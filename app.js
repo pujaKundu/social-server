76c05e59184eb7aa21cf1cd5026965002280ca32
pujaKundu <iampujakundu@gmail.com>
2023-03-04 23:43:33 +0600

    upload

--- app.js
+++ app.js
@@ -26,29 +26,30 @@ mongoose
   })
   .catch((e) => console.log(e));
 
+
+  app.use("/images", express.static(path.join(__dirname, "public/images")));
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-app.use("/images",express.static(path.join(__dirname,'public/images')))
 
 const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
-    cb(null,'public/images')
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    cb(null,file.req.body.name)
-  }
-})
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage})
-app.post('/api/upload', upload.single('file'), (req, res) => {
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
-    return res.status(200).json('File uploaded successfully')
+    return res.status(200).json("File uploded successfully");
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
-})
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
